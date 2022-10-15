e4affd39a0bf967e71b921c4375270b3a8e39c09
Joyce Chan <joycehwchan@gmail.com>
2022-10-15 16:13:19 +0900

    set login info and add 'back to home' on show.html

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+
+  http_basic_authenticate_with name: "odinproject", password: "password", except: [:index, :show]
+
   def index
     @articles = Article.all
   end
